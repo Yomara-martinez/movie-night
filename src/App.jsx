3552be43515c06a158b22634067579b0cd728c66
Yomara <yomara024@outlook.com>
2026-06-15 16:15:21 -0400

    add movie titles and genre and year

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 const initialMovies = [
-  { id: 1, title: "The Matrix",                         genre: "Sci-Fi",    year: 1999, watched: false },
-  { id: 2, title: "Parasite",                           genre: "Thriller",  year: 2019, watched: false },
-  { id: 3, title: "Everything Everywhere All at Once",  genre: "Sci-Fi",    year: 2022, watched: true  },
-  { id: 4, title: "Knives Out",                         genre: "Mystery",   year: 2019, watched: false },
-  { id: 5, title: "Coco",                               genre: "Animation", year: 2017, watched: true  },
-  { id: 6, title: "Get Out",                            genre: "Horror",    year: 2017, watched: false },
-]
+  { id: 1, title: "The Matrix", genre: "Sci-Fi", year: 1999, watched: false },
+  { id: 2, title: "Parasite", genre: "Thriller", year: 2019, watched: false },
+  {
+    id: 3,
+    title: "Everything Everywhere All at Once",
+    genre: "Sci-Fi",
+    year: 2022,
+    watched: true,
+  },
+  { id: 4, title: "Knives Out", genre: "Mystery", year: 2019, watched: false },
+  { id: 5, title: "Coco", genre: "Animation", year: 2017, watched: true },
+  { id: 6, title: "Get Out", genre: "Horror", year: 2017, watched: false },
+];
 
 export default function App() {
-    
-    const [movies, setMovies] = useState(initialMovies);
+  const [movies, setMovies] = useState(initialMovies);
 
-    
   return (
-
     <div>
-        <h1>Movie Night</h1>
+      <h1>Movie Night</h1>
+
+      {movies.map((movie) => (
+        <div key={movie.id}> {movie.title}: {movie.genre}: {movie.year}: {movie.watched}</div>
+      ))}
     </div>
-  )
+  );
 }
